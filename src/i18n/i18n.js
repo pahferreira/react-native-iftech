c4e351eb006a17de2bc52408d6577fd9eee8e7d7
Paulo Henrique <pah.dsf@gmail.com>
2019-11-24 13:05:38 -0300

    Update i18n.js

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -2,10 +2,8 @@ import I18n from 'react-native-i18n';
 import en from './languages/en-US';
 import ptBR from './languages/pt-BR';
 
-I18n.fallbacks = true;
-
 I18n.defaultLocale = 'en';
-
+I18n.fallbacks = true;
 I18n.translations = {
   en,
   'pt-BR': ptBR,
